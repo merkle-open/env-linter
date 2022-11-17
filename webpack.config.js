446516314f35bcf59fc54d5c7ab591b13048ec85
Danijel Malinovic <danijel.malinovic@emea.merkleinc.com>
2022-11-17 10:31:25 +0100

    chore: update webpack and other package versions #22

--- webpack.config.js
+++ webpack.config.js
@@ -3,22 +3,24 @@ const CleanWebpackPlugin = require('clean-webpack-plugin').CleanWebpackPlugin;
 const TsConfigWebpackPlugin = require('ts-config-webpack-plugin');
 
 module.exports = {
+	mode: 'production',
 	target: 'node',
 	node: {
 		__dirname: false,
 		__filename: false,
 	},
 	resolve: {
+		extensions: ['.js', '.ts' ],
 		alias: {
 			// override any-observable in listr because it uses require in runtime
 			'any-observable': path.resolve('./src/observable.js'),
 		},
 	},
 	output: {
-		filename: `index.js`,
 		library: 'env-linter',
 		libraryTarget: 'umd',
 		umdNamedDefine: true,
+		path: path.resolve(__dirname, "dist")
 	},
 	plugins: [
 		// Cleans the dist folder before the build starts
