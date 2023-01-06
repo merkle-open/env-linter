cd3764235b5b0473625ad3c4dadb9cd9af330597
Danijel Malinovic <danijel.malinovic@emea.merkleinc.com>
2023-01-06 13:32:26 +0100

    chore: update webpack configuration for new webpack version #22

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,84 @@
 const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin').CleanWebpackPlugin;
-const TsConfigWebpackPlugin = require('ts-config-webpack-plugin');
+const os = require("os");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 
-module.exports = {
-	target: 'node',
-	node: {
-		__dirname: false,
-		__filename: false,
-	},
-	resolve: {
-		alias: {
-			// override any-observable in listr because it uses require in runtime
-			'any-observable': path.resolve('./src/observable.js'),
+module.exports = (env) => {
+	return {
+		mode: 'production',
+		target: 'node',
+		node: {
+			__dirname: false,
+			__filename: false
 		},
-	},
-	output: {
-		filename: `index.js`,
-		library: 'env-linter',
-		libraryTarget: 'umd',
-		umdNamedDefine: true,
-	},
-	plugins: [
-		// Cleans the dist folder before the build starts
-		new CleanWebpackPlugin(),
-		// Multi threading typescript loader configuration with caching for .ts and .tsx files
-		// see https://github.com/merkle-open/webpack-config-plugins/tree/master/packages/ts-config-webpack-plugin/config
-		new TsConfigWebpackPlugin(),
-	],
+		resolve: {
+			extensions: ['.js', '.ts'],
+			alias: {
+				// override any-observable in listr because it uses require in runtime
+				'any-observable': path.resolve('./src/observable.js'),
+			},
+		},
+		output: {
+			filename: 'index.js',
+			library: {
+				name: 'env-linter',
+				type: 'umd',
+				umdNamedDefine: true
+			},
+			path: path.resolve(__dirname, "dist"),
+			clean: true
+		},
+		cache: {
+			type: 'filesystem',
+			cacheDirectory: path.resolve(__dirname, '../.cache-loader')
+		},
+		module: {
+			rules: [
+				{
+					// .ts, .tsx, .d.ts
+					test: /\.(tsx?|d.ts)$/,
+					use: [
+						{
+							// run compilation threaded
+							loader: require.resolve('thread-loader'),
+							options: {
+								// there should be 1 cpu for the fork-ts-checker-webpack-plugin
+								workers: os.cpus().length - 1,
+							},
+						},
+						{
+							// main typescript compilation loader
+							loader: require.resolve('ts-loader'),
+							options: {
+								/**
+								 * Increase build speed by disabling typechecking for the
+								 * main process and is required to be used with thread-loader
+								 * @see https://github.com/TypeStrong/ts-loader/blob/master/examples/thread-loader/webpack.config.js
+								 * Requires to use the ForkTsCheckerWebpack Plugin
+								 */
+								happyPackMode: true,
+								// Set the tsconfig.json path
+								configFile: path.resolve(__dirname, 'tsconfig.json')
+							},
+						},
+					],
+				}
+			],
+		},
+		plugins: [
+			// Multi threading typescript loader configuration with caching for .ts and .tsx files
+			// see https://github.com/merkle-open/webpack-config-plugins/tree/master/packages/ts-config-webpack-plugin/config
+			new ForkTsCheckerWebpackPlugin({
+				// block webpack's emit to wait for type checker/linter and to add errors to the webpack's compilation
+				// also required for the the overlay functionality of webpack-dev-server
+				async: env !== 'production',
+				typescript: {
+					diagnosticOptions: {
+						semantic: true,
+						syntactic: true,
+					},
+					configFile: path.resolve(__dirname, 'tsconfig.json'),
+				},
+			}),
+		],
+	}
 };
